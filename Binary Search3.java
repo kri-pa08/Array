028846f6fbeb17c037b59c1e9474a1454f236c08
kri-pa08 <kriparani410@gmail.com>
2025-11-16 00:47:55 +0530

    Mountain Peak Element Leetcode

--- Binary Search3.java
+++ Binary Search3.java
@@ -1,6 +1,32 @@
-public class Binary Search3 {
+//Mountain Peak Problem Leetcode
 
+class BinarySearch{
+ public int peakPoint(int arr[]){
+    int start = 0;
+    int end = arr.length-1;
 
-    //binary search code
-    
+  
+
+    while(start<end){
+          int mid = start + (end - start )/2;
+
+          if(arr[mid]<arr[mid+1]){
+            start = mid+1;
+
+          } 
+          else {
+            end = mid;
+          }
+
+    }
+    return start;
+ }
+
+ public static void main(String[] args){
+    int arr[] = {3, 4, 5, 1};
+
+    BinarySearch obj = new BinarySearch();
+    int start = obj.peakPoint(arr);
+    System.out.print("Peak Element is: " + start);
+ }
 }
